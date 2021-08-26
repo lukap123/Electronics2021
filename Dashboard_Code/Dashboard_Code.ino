e523d7e4da18e74e2dd19c0737c9efb5046d1b37
Luka Phillips <lukaphillips2004@gmail.com>
2021-08-26 23:48:48 +1200

    Bug Fixes

--- Dashboard_Code/Dashboard_Code.ino
+++ Dashboard_Code/Dashboard_Code.ino
@@ -1,17 +1,54 @@
+/*
+ An example showing 'ring' analogue meter on a HX8357 TFT
+ colour screen
+
+ Needs Fonts 2, 4 and 7 (also Font 6 if using a large size meter)
+ */
+
+// Meter colour schemes
+#define RED2RED 0
+#define GREEN2GREEN 1
+#define BLUE2BLUE 2
+#define BLUE2RED 3
+#define GREEN2RED 4
+#define RED2GREEN 5
+
+// #include <avr/dtostrf.h> // Only needed for Due
+
+#include <TFT_HX8357.h> // Hardware-specific library
+
+TFT_HX8357 tft = TFT_HX8357();       // Invoke custom library
+
+#define HX8357_GREY 0x2104 // Dark grey 16 bit colour
+
+uint32_t runTime = -99999;       // time for next update
+
+int reading = 0; // Value to be displayed
+int d = 0; // Variable used for the sinewave test waveform
+boolean alert = 0;
+int8_t ramp = 1;
+
 #define CHANGE 1
 #define FALLING 2
 #define RISING 3
 
 unsigned long start, finished;
-const int debounce = 200;  // debounce in milliseconds
+const int debounce = 10;  // debounce in milliseconds
 unsigned long now = 0;
 
 byte Rotations = 0;
+byte Speed = 0;
 
-void setup()
-{
+void setup(void) {
+  tft.begin();
+  //Serial.begin(9600);
+  tft.setRotation(1);
+
+  tft.fillScreen(HX8357_BLACK);
+
+  
   pinMode(18, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(18), speedCalc, FALLING); // interrupt called when falling edge is detected on digital pin 18
+  attachInterrupt(digitalPinToInterrupt(18), speedCalc, RISING); // interrupt called when falling edge is detected on digital pin 18
   start = millis();
   delay(3000);
   Serial.begin(9600);
@@ -36,28 +73,200 @@ void setup()
 }
 
 
+void loop() {
+  if (millis() - runTime >= 0L) { // Execute every 2s
+    runTime = millis();
+
+    // Test with a slowly changing value from a Sine function
+    //d += 4; if (d >= 360) d = 0;
+
+    // Set the the position, gap between meters, and inner radius of the meters
+    int xpos = 0, ypos = 5, gap = 4, radius = 52;
+
+
+
+    // Draw a large meter
+    xpos = 480/2 - 160, ypos = 0, gap = 15, radius = 170;
+    reading = Speed;
+    // Comment out above meters, then uncomment the next line to show large meter
+    ringMeter(reading,0,100, xpos,ypos,radius,"KPH",GREEN2RED); // Draw analogue meter
+
+  }
+}
+
 void speedCalc()
 {
   now = millis();
   if (now - start > debounce)
   {
     Rotations++ ;
+    Serial.println (Rotations);
     start = now;
   }
 }
 
 ISR(TIMER1_COMPA_vect) 
 {
-  Serial.println(Rotations);
+  Speed = Rotations;
+  Serial.println (Speed);
   Rotations = 0;
 }
 
 
 
-
-void loop()
+// #########################################################################
+//  Draw the meter on the screen, returns x coord of righthand side
+// #########################################################################
+int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *units, byte scheme)
 {
+  // Minimum value of r is about 52 before value text intrudes on ring
+  // drawing the text first is an option
+  
+  x += r; y += r;   // Calculate coords of centre of ring
 
-  delay (500);
+  int w = r / 3;    // Width of outer ring is 1/4 of radius
+  
+  int angle = 150;  // Half the sweep angle of meter (300 degrees)
 
+  int v = map(value, vmin, vmax, -angle, angle); // Map the value to an angle v
+
+  byte seg = 3; // Segments are 3 degrees wide = 100 segments for 300 degrees
+  byte inc = 6; // Draw segments every 3 degrees, increase to 6 for segmented ring
+
+  // Variable to save "value" text colour from scheme and set default
+  int colour = HX8357_BLUE;
+ 
+  // Draw colour blocks every inc degrees
+  for (int i = -angle+inc/2; i < angle-inc/2; i += inc) {
+    // Calculate pair of coordinates for segment start
+    float sx = cos((i - 90) * 0.0174532925);
+    float sy = sin((i - 90) * 0.0174532925);
+    uint16_t x0 = sx * (r - w) + x;
+    uint16_t y0 = sy * (r - w) + y;
+    uint16_t x1 = sx * r + x;
+    uint16_t y1 = sy * r + y;
+
+    // Calculate pair of coordinates for segment end
+    float sx2 = cos((i + seg - 90) * 0.0174532925);
+    float sy2 = sin((i + seg - 90) * 0.0174532925);
+    int x2 = sx2 * (r - w) + x;
+    int y2 = sy2 * (r - w) + y;
+    int x3 = sx2 * r + x;
+    int y3 = sy2 * r + y;
+
+    if (i < v) { // Fill in coloured segments with 2 triangles
+      switch (scheme) {
+        case 0: colour = HX8357_RED; break; // Fixed colour
+        case 1: colour = HX8357_GREEN; break; // Fixed colour
+        case 2: colour = HX8357_BLUE; break; // Fixed colour
+        case 3: colour = rainbow(map(i, -angle, angle, 0, 127)); break; // Full spectrum blue to red
+        case 4: colour = rainbow(map(i, -angle, angle, 70, 127)); break; // Green to red (high temperature etc)
+        case 5: colour = rainbow(map(i, -angle, angle, 127, 63)); break; // Red to green (low battery etc)
+        default: colour = HX8357_BLUE; break; // Fixed colour
+      }
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, colour);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, colour);
+      //text_colour = colour; // Save the last colour drawn
+    }
+    else // Fill in blank segments
+    {
+      tft.fillTriangle(x0, y0, x1, y1, x2, y2, HX8357_GREY);
+      tft.fillTriangle(x1, y1, x2, y2, x3, y3, HX8357_GREY);
+    }
+  }
+  // Convert value to a string
+  char buf[10];
+  byte len = 3; if (value > 999) len = 5;
+  dtostrf(value, len, 0, buf);
+  buf[len] = ' '; buf[len+1] = 0; // Add blanking space and terminator, helps to centre text too!
+  // Set the text colour to default
+  tft.setTextSize(1);
+
+  if (value<vmin || value>vmax) {
+    drawAlert(x,y+90,50,1);
+  }
+  else {
+    drawAlert(x,y+90,50,0);
+  }
+
+  tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
+  // Uncomment next line to set the text colour to the last segment value!
+  tft.setTextColor(colour, HX8357_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  // Print value, if the meter is large then use big font 8, othewise use 4
+  if (r > 84) {
+    tft.setTextPadding(55*3); // Allow for 3 digits each 55 pixels wide
+    tft.drawString(buf, x, y, 8); // Value in middle
+  }
+  else {
+    tft.setTextPadding(3 * 14); // Allow for 3 digits each 14 pixels wide
+    tft.drawString(buf, x, y, 4); // Value in middle
+  }
+  tft.setTextSize(1);
+  tft.setTextPadding(0);
+  // Print units, if the meter is large then use big font 4, othewise use 2
+  tft.setTextColor(HX8357_WHITE, HX8357_BLACK);
+  if (r > 84) tft.drawString(units, x, y + 60, 4); // Units display
+  else tft.drawString(units, x, y + 15, 2); // Units display
+
+  // Calculate and return right hand side x coordinate
+  return x + r;
+}
+
+void drawAlert(int x, int y , int side, boolean draw)
+{
+  if (draw && !alert) {
+    tft.fillTriangle(x, y, x+30, y+47, x-30, y+47, rainbow(95));
+    tft.setTextColor(HX8357_BLACK);
+    tft.drawCentreString("!", x, y + 6, 4);
+    alert = 1;
+  }
+  else if (!draw) {
+    tft.fillTriangle(x, y, x+30, y+47, x-30, y+47, HX8357_BLACK);
+    alert = 0;
+  }
+}
+
+// #########################################################################
+// Return a 16 bit rainbow colour
+// #########################################################################
+unsigned int rainbow(byte value)
+{
+  // Value is expected to be in range 0-127
+  // The value is converted to a spectrum colour from 0 = blue through to 127 = red
+
+  byte red = 0; // Red is the top 5 bits of a 16 bit colour value
+  byte green = 0;// Green is the middle 6 bits
+  byte blue = 0; // Blue is the bottom 5 bits
+
+  byte quadrant = value / 32;
+
+  if (quadrant == 0) {
+    blue = 31;
+    green = 2 * (value % 32);
+    red = 0;
+  }
+  if (quadrant == 1) {
+    blue = 31 - (value % 32);
+    green = 63;
+    red = 0;
+  }
+  if (quadrant == 2) {
+    blue = 0;
+    green = 63;
+    red = value % 32;
+  }
+  if (quadrant == 3) {
+    blue = 0;
+    green = 63 - 2 * (value % 32);
+    red = 31;
+  }
+  return (red << 11) + (green << 5) + blue;
+}
+
+// #########################################################################
+// Return a value in range -1 to +1 for a given phase angle in degrees
+// #########################################################################
+float sineWave(int phase) {
+  return sin(phase * 0.0174532925);
 }
